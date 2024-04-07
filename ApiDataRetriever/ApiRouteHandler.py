1cee658655fa4a3610e238524081493d50754a46
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2024-04-07 16:21:15 -0700

    Typo fix for api route handler inherited method

--- ApiDataRetriever/ApiRouteHandler.py
+++ ApiDataRetriever/ApiRouteHandler.py
@@ -2,5 +2,5 @@ class ApiRouteHandler:
     def __init__(self, key: str):
         self.key = key
 
-    def getFullApiPath(self, route: str) -> str:
+    def _getFullApiPath(self, route: str) -> str:
         raise NotImplementedError("Please implement this method")
