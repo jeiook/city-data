fda3ed367687bce216a256606a7496867a3f19a8
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2024-04-14 19:46:38 -0700

    Extract rent data of region over range of dates

--- FileDataExtractor/ZoriCSVFileDataExtractor.py
+++ FileDataExtractor/ZoriCSVFileDataExtractor.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import csv
+from datetime import date
 
 
 class ZoriCSVFileDataExtractor:
@@ -12,12 +13,35 @@ class ZoriCSVFileDataExtractor:
             if propertyName[0].isnumeric():
                 firstMonthIndex = i
                 break
-        self._months = properties[firstMonthIndex:]
+        self._months = [date.fromisoformat(
+            monthString) for monthString in properties[firstMonthIndex:]]
         properties = properties[:firstMonthIndex] + ["RentPricesByMonth"]
-        listOfData = [line[:firstMonthIndex] +
-                      [tuple(line[firstMonthIndex:])] for line in reader if line]
+        listOfDataValues = [line[:firstMonthIndex] +
+                            [tuple(line[firstMonthIndex:])] for line in reader if line]
         constructor = namedtuple("ZoriCSVFileDataObject", properties)
-        self._data = [constructor._make(data) for data in listOfData]
+        self._dataList = [constructor._make(data) for data in listOfDataValues]
 
-    def get_data(self):
-        return self._data
+    def _getDataByRegion(self, regionName: str):
+        return [data for data in self._dataList if data.RegionName == regionName][0]
+
+    def _findMonthsRangeIndices(self, startDate: date, endDate: date):
+        startIndex, endIndex = -1, -1
+        for i, dateValue in enumerate(self._months):
+            if startIndex != -1 and endIndex != -1:
+                break
+            if startIndex == -1 and dateValue == startDate:
+                startIndex = i
+                continue
+            if endIndex == -1 and dateValue == endDate:
+                endIndex = i
+                continue
+        if startIndex == -1:
+            raise ValueError(f'Invalid starting date {str(startDate)}')
+        if endIndex == -1:
+            raise ValueError(f'Invalid ending date {str(endDate)}')
+        return (startIndex, endIndex)
+
+    def getRentsOfRegionOverRange(self, regionName: str, startDate: date = date.fromisoformat("2015-01-31"), endDate: date = date.fromisoformat("2024-02-29")):
+        data = self._getDataByRegion(regionName)
+        startIndex, endIndex = self._findMonthsRangeIndices(startDate, endDate)
+        return list(map(lambda x: float(x) if x else 0, filter(lambda s: len(s), data.RentPricesByMonth[startIndex:endIndex])))
