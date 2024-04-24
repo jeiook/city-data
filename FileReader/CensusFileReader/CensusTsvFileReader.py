24327dd2072c6a129a2ae07fcb47bb30ea9de255
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2024-04-23 17:01:49 -0700

    Deleted obsolete FileReaders and DataProcessors

--- FileReader/CensusFileReader/CensusTsvFileReader.py
+++
@@ -1,9 +0,0 @@
-from FileReader.FileReader import FileReader
-
-
-class CensusTsvFileReader(FileReader):
-    def __init__(self, fileName: str):
-        super().__init__(fileName)
-
-    def splitLineIntoTokens(self, line: str) -> list[str]:
-        return line.strip().split("\t")
--- main.py
+++ main.py
@@ -2,39 +2,25 @@ from ApiDataRetriever.ApiDataRetriever import ApiDataRetriever
 from ApiDataRetriever.Census.CensusApiDataType import CensusApiData
 from ApiDataRetriever.Census.CensusApiRouteHandler import CensusApiRouteHandler
 from src.DataExplorer.CensusStateMappingsDataExplorer import CensusStateMappingsDataExplorer
-from src.DataExtractor.DataExtractor import DataExtractor
-from src.DataExtractor.DataObject.DataObjectBuilder.DictionaryDataObjectBuilder import DictionaryDataObjectBuilder
-from src.DataExtractor.SourceDataRetriever.CsvFileSourceDataRetriever import CsvFileSourceDataRetriever
-from DataProcessor.CityZoriDataProcessor import CityZoriDataProcessor
-from DataProcessor.MetroZoriDataProcessor import MetroZoriDataProcessor
-from DataProcessor.PlacesInMetroDataProcessor import PlacesInMetroDataProcessor
-from FileReader.CensusFileReader.CensusTsvFileReader import CensusTsvFileReader
-from FileReader.ZoriFileReader.ZoriFileReader import ZoriFileReader
-from OutputHandler.FileOutputHandler import FileOutputHandler
 
 
 def zoriMetroDriver():
-    fileReader = ZoriFileReader("data/zori_metro.csv")
-    dataProcessor = MetroZoriDataProcessor(
-        fileReader.getDataDictFromFile())
     yearStart = 2022
     yearEnd = 2024
     metroNames = ["rochester", "baltimore", "madison", "st. louis", "chicago", "buffalo", "minneapolis", "philadelphia",
                   "pittsburgh", "new york", "salt lake city", "boston", "portland", "san francisco", "seattle", "los angeles", "austin, tx"]
-    for metroName in metroNames:
-        row = dataProcessor.getRowByLocationName(metroName)
-        print(metroName, "-", dataProcessor.getAverageZORIOfRowOverRange(
-            row, yearStart, yearEnd))
+    # for metroName in metroNames:
+    #     row = dataProcessor.getRowByLocationName(metroName)
+    #     print(metroName, "-", dataProcessor.getAverageZORIOfRowOverRange(
+    #         row, yearStart, yearEnd))
     print("results from zori_metro.csv")
 
 
 def zoriCityDriver():
-    fileReader = ZoriFileReader("data/zori_city.csv")
-    dataProcessor = CityZoriDataProcessor(fileReader.getDataDictFromFile())
     yearStart = 2022
     yearEnd = 2024
-    dataProcessor.listAverageZORIOfLocationsInMetroOverRange(
-        "los angeles", yearStart, yearEnd)
+    # dataProcessor.listAverageZORIOfLocationsInMetroOverRange(
+    #     "los angeles", yearStart, yearEnd)
     print("results from zori_city.csv")
 
 
